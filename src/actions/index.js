418f8d78a3f152cc23d631aed81db59b18d4666e
khaoula <mahfoudhikhaoula@gmail.com>
2020-10-26 09:50:43 +0100

    second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -30,3 +30,4 @@ export const addnewTodo = (todo) => {
       payload:todo
     };
   };
+  
--- src/components/ListOfTask.js
+++ src/components/ListOfTask.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "./Task";
 import { useSelector } from "react-redux";
+import Button from "react-bootstrap/Button";
 
 const ListOfTask = () => {
-  
   let todos = useSelector((state) => state);
+  const DoneTaskslist = useSelector((state) =>
+    state.filter((todo) => todo.isDone)
+  );
+  const UnDoneTaskslist = useSelector((state) =>
+    state.filter((todo) => todo.isDone === false)
+  );
+  const [filterDone, setFilterDone] = useState(false);
+  const [filterUnDone, setFilterUnDone] = useState(false);
+
+  const filterDoneTasks = () => {
+    setFilterUnDone(false);
+    setFilterDone(true);
+  };
+  const filterUnDoneTasks = () => {
+    setFilterDone(false);
+    setFilterUnDone(true);
+  };
+  const showAll = () => {
+    setFilterUnDone(false);
+    setFilterDone(false);
+  };
   return (
-    
-       <div className ="container">
-        {todos.length > 0 &&
-          todos.map((todo) => <Task key={todo.TaskId} todo={todo} />)}
+    <div>
+      <div>
+        <Button variant="primary" onClick={filterDoneTasks}>
+          Done Tasks
+        </Button>
+        <Button variant="primary" onClick={filterUnDoneTasks}>
+          Undone Tasks
+        </Button>
+        <Button variant="primary" onClick={showAll}>
+          All Tasks
+        </Button>
       </div>
-      
-     
+      <div className="container">
+        {(filterDone || filterUnDone) === false
+          ? todos.map((todo) => <Task key={todo.TaskId} todo={todo} />)
+          : null}
+        {filterDone
+          ? DoneTaskslist.map((todo) => <Task key={todo.TaskId} todo={todo} />)
+          : null}
+        {filterUnDone
+          ? UnDoneTaskslist.map((todo) => (
+              <Task key={todo.TaskId} todo={todo} />
+            ))
+          : null}
+      </div>
+    </div>
   );
 };
 
